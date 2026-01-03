530d276f43ece980347801239394734813bfdbb8
Rikhil Arora <rikhilcooldude@gmail.com>
2026-01-03 07:21:23 +0100

    feat show cart items in header

--- packages/container/src/Header.jsx
+++ packages/container/src/Header.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
-
+import { useCart } from "cart/CartContext";
 
 export default function Header() {
+  const { items } = useCart();
     return (
     <div className="header">
       <h1>Rizz Phones</h1>
       <nav className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
-        <Link to="/cart">Cart</Link>
+        <Link to="/cart">Cart ({items.length})</Link>
       </nav>
     </div>
     )
